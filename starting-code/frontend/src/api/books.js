4e28e5041d720a630a062a8a7855a19d0d7e299f
Heidi Smith <heidi.smith@ramseysolutions.com>
2026-04-23 14:59:52 -0500

    Fix template literal in api calls

--- starting-code/frontend/src/api/books.js
+++ starting-code/frontend/src/api/books.js
@@ -17,14 +17,14 @@ export const addNewBook = async (newTitle, newStart, newEnd) => {
 };
 // TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
 export const getBooks = async () => {
-    const response = await fetch('${API_ENDPOINT}/books');
+    const response = await fetch(`${API_ENDPOINT}/books`);
     const books = await response.json();
 
     return books;
 };
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
 export const updateBook = async (id, newTitle, newStart, newEnd) => {
-    const response = await fetch('${API_ENDPOINT}/books/${id}', {
+    const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
             newTitle,
@@ -39,7 +39,7 @@ export const updateBook = async (id, newTitle, newStart, newEnd) => {
 }
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
 export const deleteBook = async (id) => {
-    const response = await fetch('${API_ENDPOINT}/books/${id}', {
+    const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
         method: 'DELETE',
     });
     return response.status;
